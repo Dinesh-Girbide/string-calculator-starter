b1045dde5caaacde1ddae7768c264906e891c676
Dinesh-Girbide <85330597+Dinesh-Girbide@users.noreply.github.com>
2021-10-17 00:24:19 +0530

    First two default cases working

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,6 +3,10 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
+        if(numbers.isEmpty())
+	    	 return 0;
+	     if(numbers.length()==1 && Character.isDigit(numbers.charAt(0)))
+	    	 return Integer.parseInt(numbers);
         return 0;
     }
 
