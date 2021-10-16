18b70c7b16211a464df40435e2d663327847c155
Dinesh-Girbide <85330597+Dinesh-Girbide@users.noreply.github.com>
2021-10-17 00:25:16 +0530

    changed string arg from input to numbers

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,7 +2,7 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
+    public int add(String numbers) {
         if(numbers.isEmpty())
 	    	 return 0;
 	     if(numbers.length()==1 && Character.isDigit(numbers.charAt(0)))
