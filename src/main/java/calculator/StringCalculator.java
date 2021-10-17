af9bcc65cbad770a4513a77630d2a1f13e9141d9
Dinesh-Girbide <ddgirbide@gmail.com>
2021-10-17 23:28:08 +0530

    Final Code Submission with GitHUb Action

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,19 +2,25 @@ package calculator;
 
 class StringCalculator {
 	private StringBuilder defaultDelimiter;
+	// checking how many times add method is called(not per object but globally
+	// count of how many times add method is called)
 	private static int addMethodInvokedCount;
 
+	// Constructor for initialize object data members of StringCalculator class
 	public StringCalculator() {
 		defaultDelimiter = new StringBuilder("\n|,");
 	}
 
 	public int add(String numbers) throws Exception {
 		addMethodInvokedCount++;
+		// if numbers is empty returning 0
 		if (numbers.isEmpty())
 			return 0;
+		// if numers lenght is 1 then return given number from string
 		if (numbers.length() == 1 && Character.isDigit(numbers.charAt(0)))
 			return stringToInt(numbers);
 		else {
+			// if string is starting with '//['
 			if (numbers.startsWith("//[")) {
 				int numOfDelimiter = 0;
 				int len = 0;
@@ -39,7 +45,7 @@ class StringCalculator {
 					}
 				}
 				numbers = numbers.substring(len * numOfDelimiter + 3 + numOfDelimiter * 2, numbers.length());
-			} else if (numbers.startsWith("//"))
+			} else if (numbers.startsWith("//")) // if string starts with only '//'
 
 			{
 				defaultDelimiter.append("|\\" + numbers.charAt(2));
@@ -54,6 +60,8 @@ class StringCalculator {
 
 	}
 
+	// for adding numbers which are obtained by spliting String on the basis of
+	// delimiter
 	private int sumOfNumbers(String[] nums) {
 		int sum = 0;
 		for (String n : nums) {
@@ -62,26 +70,26 @@ class StringCalculator {
 		return sum;
 	}
 
-	private String checkNegativeNumbers(String[] nums) throws Exception {
+//geting negative numbers from string and throwing exception
+	private void checkNegativeNumbers(String[] nums) throws Exception {
 		String negativeNums = "";
-		int count = 0;
 		for (String n : nums) {
 			if (stringToInt(n) < 0) {
-				count++;
 				negativeNums += n + ",";
 			}
 		}
-		if (count == 1)
+		// if negative numbers are there in string then exception will be thrown with
+		// message as "negatives not allowed"
+		if (negativeNums.length() > 0)
 			throw new Exception("negatives not allowed =>  " + negativeNums);
-		else if (count > 1)
-			throw new Exception("negatives not allowed =>  " + negativeNums);
-		return negativeNums;
 	}
 
+//converting string to integer
 	private int stringToInt(String num) {
 		return Integer.parseInt(num);
 	}
 
+//geting how many number of times add method is called
 	public int GetCalledCount() {
 		return addMethodInvokedCount;
 	}
