cd404d40d241d4bc82816bc1977db9aeb84be218
Dinesh-Girbide <ddgirbide@gmail.com>
2021-10-17 15:59:38 +0530

    test for checking negative numbers

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,22 +3,43 @@ package calculator;
 class StringCalculator {
 	private final String defaultDelimiter = ",|\n";
 
-	public int add(String numbers) {
+	public int add(String numbers) throws Exception {
 
 		String[] nums = numbers.split(defaultDelimiter);
-		int sum = 0;
+
 		if (numbers.isEmpty())
 			return 0;
 		if (numbers.length() == 1 && Character.isDigit(numbers.charAt(0)))
 			return stringToInt(numbers);
 		else {
-
-			for (String n : nums) {
-				sum += stringToInt(n);
-			}
+			checkNegativeNumbers(nums);
+			return sumOfNumbers(nums);
 
 		}
+	}
+
+	private int sumOfNumbers(String[] nums) {
+		int sum = 0;
+		for (String n : nums) {
+			sum += stringToInt(n);
+		}
 		return sum;
+	}
+
+	private String checkNegativeNumbers(String[] nums) throws Exception {
+		String negativeNums = "";
+		int count = 0;
+		for (String n : nums) {
+			if (stringToInt(n) < 0) {
+				count++;
+				negativeNums += n + ",";
+			}
+		}
+		if (count == 1)
+			throw new Exception("negatives not allowed");
+		else if (count > 1)
+			throw new Exception("negatives not allowed " + negativeNums);
+		return negativeNums;
 	}
 
 	private int stringToInt(String num) {
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -7,26 +7,36 @@ import static org.junit.jupiter.api.Assertions.*;
 class StringCalculatorShould {
 
     @Test
-    void empty_string_should_return_0() {
+    void empty_string_should_return_0() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
-    void string_with_single_number_should_return_number_as_int() {
+    void string_with_single_number_should_return_number_as_int() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
     
     @Test
-	void string_with_two_numbers_should_return_numbers_sum_as_int() {
+	void string_with_two_numbers_should_return_numbers_sum_as_int() throws Exception {
 		StringCalculator stringCalculator=new StringCalculator();
 		assertEquals(6,stringCalculator.add("2\n4"));
 	}
 	
 	@Test
-	void string_with_three_numbers_should_return_numbers_sum_as_int() {
+	void string_with_three_numbers_should_return_numbers_sum_as_int() throws Exception {
 		StringCalculator stringCalculator=new StringCalculator();
 		assertEquals(7,stringCalculator.add("1,2\n4"));
+	}
+	
+	@Test
+	void string_with_negative_number_should_throw_exception(){
+		StringCalculator stringCalculator=new StringCalculator();
+		try {
+			assertEquals(3,stringCalculator.add("-3,-3"));
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
