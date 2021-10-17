8d46b85d16292305647e63087846a53eedd78dfc
Dinesh-Girbide <ddgirbide@gmail.com>
2021-10-17 20:44:05 +0530

    Final - All test Cases Working Properly

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,46 +2,56 @@ package calculator;
 
 class StringCalculator {
 	private StringBuilder defaultDelimiter;
+	private static int addMethodInvokedCount;
 
 	public StringCalculator() {
 		defaultDelimiter = new StringBuilder("\n|,");
 	}
 
 	public int add(String numbers) throws Exception {
+		addMethodInvokedCount++;
 		if (numbers.isEmpty())
 			return 0;
 		if (numbers.length() == 1 && Character.isDigit(numbers.charAt(0)))
 			return stringToInt(numbers);
 		else {
-			if (numbers.startsWith("//")) {
-				StringBuilder sb = new StringBuilder();
+			if (numbers.startsWith("//[")) {
+				int numOfDelimiter = 0;
 				int len = 0;
-				for (int i = 2; i < numbers.length() - 1; i++) {
-					if (numbers.charAt(i) == '\n') {
-						break;
-					} else {
-						char c = numbers.charAt(i);
-						len++;
-						if (c == '+' || c == '-' || c == '*' || c == '/' || c == '%') {
-							sb.append('\\');
-							sb.append(numbers.charAt(i));
-						} else {
-							sb.append(numbers.charAt(i));
-						}
+				for (int j = 2; j < numbers.length(); j++) {
 
+					if (numbers.charAt(j) == '\n')
+						break;
+
+					if (numbers.charAt(j) == '[') {
+						numOfDelimiter++;
+						StringBuilder sb = new StringBuilder();
+						len = 0;
+						for (int i = j + 1; i < numbers.length(); i++) {
+							if (numbers.charAt(i) == ']') {
+								break;
+							} else {
+								len++;
+								sb.append("\\" + numbers.charAt(i));
+							}
+						}
+						defaultDelimiter.append("|" + sb);
 					}
 				}
+				numbers = numbers.substring(len * numOfDelimiter + 3 + numOfDelimiter * 2, numbers.length());
+			} else if (numbers.startsWith("//"))
 
-				numbers = numbers.substring(len + 3, numbers.length());
-				defaultDelimiter.append("|" + sb);
-			} else if (numbers.startsWith("//[")) {
-
+			{
+				defaultDelimiter.append("|\\" + numbers.charAt(2));
+				numbers = numbers.substring(1 + 3, numbers.length());
 			}
 			String[] nums = numbers.split(defaultDelimiter.toString());
+
 			checkNegativeNumbers(nums);
 			return sumOfNumbers(nums);
 
 		}
+
 	}
 
 	private int sumOfNumbers(String[] nums) {
@@ -62,13 +72,17 @@ class StringCalculator {
 			}
 		}
 		if (count == 1)
-			throw new Exception("negatives not allowed");
+			throw new Exception("negatives not allowed =>  " + negativeNums);
 		else if (count > 1)
-			throw new Exception("negatives not allowed " + negativeNums);
+			throw new Exception("negatives not allowed =>  " + negativeNums);
 		return negativeNums;
 	}
 
 	private int stringToInt(String num) {
 		return Integer.parseInt(num);
+	}
+
+	public int GetCalledCount() {
+		return addMethodInvokedCount;
 	}
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -1,55 +1,91 @@
 package calculator;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
 
 class StringCalculatorShould {
 
-//    @Test
-//    void empty_string_should_return_0() throws Exception {
-//        StringCalculator stringCalculator = new StringCalculator();
-//        assertEquals(0, stringCalculator.add(""));
-//    }
-//
-//    @Test
-//    void string_with_single_number_should_return_number_as_int() throws Exception {
-//        StringCalculator stringCalculator = new StringCalculator();
-//        assertEquals(1, stringCalculator.add("1"));
-//    }
-//    
-//    @Test
-//	void string_with_two_numbers_should_return_numbers_sum_as_int() throws Exception {
-//		StringCalculator stringCalculator=new StringCalculator();
-//		assertEquals(6,stringCalculator.add("2\n4"));
-//	}
-//	
-//	@Test
-//	void string_with_three_numbers_should_return_numbers_sum_as_int() throws Exception {
-//		StringCalculator stringCalculator=new StringCalculator();
-//		assertEquals(7,stringCalculator.add("1,2\n4"));
-//		
-//	}
-//	
-//	@Test
-//	void string_with_negative_number_should_throw_exception(){
-//		StringCalculator stringCalculator=new StringCalculator();
-//		try {
-//			assertEquals(3,stringCalculator.add("-3,-3"));
-//		}catch(Exception e) {
-//			System.out.println(e.getMessage());
-//		}
-//	}
-//	
-//	@Test
-//	void string_with_single_different_delimiter() throws Exception {
-//		StringCalculator stringCalculator=new StringCalculator();
-//		assertEquals(11,stringCalculator.add("//;\n2;3;6,1000"));
-//	}
-	
 	@Test
-	void string_with_single_different_group_of_delimiter() throws Exception {
-		StringCalculator stringCalculator=new StringCalculator();
-		assertEquals(26,stringCalculator.add("//::\n2::5,6::13"));
+	void empty_string_should_return_0() throws Exception {
+		StringCalculator stringCalculator = new StringCalculator();
+		assertEquals(0, stringCalculator.add(""));
+	}
+
+	@Test
+	void string_with_single_number_should_return_number_as_int() throws Exception {
+		StringCalculator stringCalculator = new StringCalculator();
+		assertEquals(1, stringCalculator.add("1"));
+	}
+
+	@Test
+	void string_with_two_numbers_should_return_numbers_sum_as_int() throws Exception {
+		StringCalculator stringCalculator = new StringCalculator();
+		assertEquals(3, stringCalculator.add("1,2"));
+	}
+
+	@Test
+	void string_with_three_numbers_should_return_numbers_sum_as_int() throws Exception {
+		StringCalculator stringCalculator = new StringCalculator();
+		assertEquals(7, stringCalculator.add("1,2\n4"));
+	}
+
+	@Test
+	void string_with_unknown_numbers_should_return_numbers_sum_as_int() throws Exception {
+		StringCalculator stringCalculator = new StringCalculator();
+		assertEquals(55, stringCalculator.add("1,2\n3,4,5\n6,7,8,9,10"));
+	}
+
+	@Test
+	void string_with_number_greater_than_1000() throws Exception {
+		StringCalculator stringCalculator = new StringCalculator();
+		assertEquals(11, stringCalculator.add("2,3,1005,6,1000"));
+	}
+
+	@Test
+	void string_with_single_different_delimiter() throws Exception {
+		StringCalculator stringCalculator = new StringCalculator();
+		assertEquals(11, stringCalculator.add("//;\n2;3;6,1000"));
+	}
+
+	@Test()
+	void string_with_negative_number_should_throw_exception() {
+		StringCalculator stringCalculator = new StringCalculator();
+		Exception exception = assertThrows(Exception.class, () -> stringCalculator.add("1,2,-3"));
+		System.out.println(exception.getMessage());
+	}
+
+	@Test
+	void string_with_multiple_negative_number_should_throw_exception() {
+		StringCalculator stringCalculator = new StringCalculator();
+		Exception exception = assertThrows(Exception.class, () -> stringCalculator.add("1,2,-4,-5,-6,8,7,-9"));
+		System.out.println(exception.getMessage());
+	}
+
+	@Test
+	void string_with_single_delimiter_of_one_length() throws Exception {
+		StringCalculator stringCalculator = new StringCalculator();
+		assertEquals(26, stringCalculator.add("//[&]\n2&5&6&13"));
+	}
+
+	@Test
+	void string_with_single_delimiter_of_any_lenght() throws Exception {
+		StringCalculator stringCalculator = new StringCalculator();
+		assertEquals(26, stringCalculator.add("//[**]\n2**5**6**13"));
+	}
+
+	@Test
+	void string_with_multiple_delimiter_of_any_lenght() throws Exception {
+		StringCalculator stringCalculator = new StringCalculator();
+		assertEquals(26, stringCalculator.add("//[**][%%]\n2**5,6%%13"));
+	}
+
+	@Test
+	@AfterAll
+	static void number_of_times_add_method_invoked() {
+		StringCalculator stringCalculator = new StringCalculator();
+		assertEquals(12, stringCalculator.GetCalledCount());
 	}
 }
